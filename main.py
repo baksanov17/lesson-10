ad1f29d651a7e92fa464bd81023dab336103b451
baksanov17 <baksanov17@mail.ru>
2024-08-30 21:45:37 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+positive_numbers = []
+i = 0
+while i < len(my_list):
+    if my_list[i] >= 0:
+        positive_numbers = positive_numbers + [my_list[i]]
+        i = i + 1
+    else:
+        break
+print(positive_numbers)
+
+
