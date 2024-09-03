ab41ecf7441d749a00a1ba54cb4ef347750a7803
baksanov17 <baksanov17@mail.ru>
2024-09-03 22:21:27 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 positive_numbers = []
 i = 0
 while i < len(my_list):
-    if my_list[i] >= 0:
+    if my_list[i] > 0:
         positive_numbers = positive_numbers + [my_list[i]]
         i = i + 1
+    elif my_list[i] == 0:
+        i = i + 1
+        continue
     else:
         break
-print(positive_numbers)
+print(','.join(map(str,positive_numbers)))
+
 
 
